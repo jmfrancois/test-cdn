971231f40d6be5b33904000fd7f7cf5b59f8aac4
jmfrancois <jmfrancois@talend.com>
2020-10-02 12:06:13 +0200

    fix: add i18next to setup tui

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Action } from '@talend/react-components';
+import i18n from 'i18next';
+import { initReactI18next } from "react-i18next";
+
+i18n.use(initReactI18next).init({
+    react: {
+        useSuspense: false
+    }
+});
 
 ReactDOM.render(
     <div>
