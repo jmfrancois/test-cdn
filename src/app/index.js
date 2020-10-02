a8f3a1480ea3d83f1726b2a48a948f3f57f84ffa
jmfrancois <jmfrancois@talend.com>
2020-10-02 08:21:04 +0200

    fix: root element

--- src/app/index.js
+++ src/app/index.js
@@ -5,8 +5,9 @@ import { Action } from '@talend/react-components';
 ReactDOM.render(
     <div>
         <h1>Hello, world!</h1>
+
         <Action onClick={(e) => console.log('clicked')} label="Hello"></Action>
     </div>,
-    document.getElementById('root')
+    document.getElementById('app')
 );
   
