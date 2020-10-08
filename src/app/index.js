3e6c21f5acefd91dd1f30163ee4543f51203a432
jmfrancois <jmfrancois@talend.com>
2020-10-08 11:59:44 +0200

    feat: add cmf/containers

--- src/app/index.js
+++ src/app/index.js
@@ -1,22 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Action } from '@talend/react-components';
-import i18n from 'i18next';
+import React from "react";
+import { Action } from "@talend/react-components";
+import i18n from "i18next";
+import cmf, { cmfConnect } from "@talend/react-cmf";
+import containersModule, { AppLoader } from "@talend/react-containers";
+// note: containers ne fonctionne pas sans react-router. ...
+import getRouter from "@talend/react-cmf-router";
 import { initReactI18next } from "react-i18next";
 
 i18n.use(initReactI18next).init({
-    react: {
-        useSuspense: false
-    }
+  react: {
+    useSuspense: false,
+  },
+});
+const router = getRouter({});
+
+const RootComponent = cmfConnect()(function MyComponent(props) {
+  return (
+    <div>
+      <h1>Hello {props.hello}</h1>
+      <Action onClick={(e) => console.log("clicked")} label="Hello"></Action>
+    </div>
+  );
 });
 
-if (document.getElementById('app')) {
-    ReactDOM.render(
-        <div>
-            <h1>Hello, world!</h1>
-    
-            <Action onClick={(e) => console.log('clicked')} label="Hello"></Action>
-        </div>,
-        document.getElementById('app')
-    );
-}
+cmf.bootstrap({
+  appId: "app",
+  components: {
+    Action,
+  },
+  AppLoader,
+  RootComponent,
+  modules: [router.cmfModule, containersModule],
+});
