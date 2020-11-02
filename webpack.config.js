8c0f66da8c4f330c59abd8a7481bc0dab2cfe9b2
jmfrancois <jmfrancois@talend.com>
2020-11-02 16:29:27 +0100

    feat: make it work over custom cdn

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const moduleToCdn = require("@talend/module-to-cdn");
 // import cdn from "@talend/scripts-config-cdn";
 
 function getURL(info) {
-  return `https://talend-cdn.surge.sh/${info.name}/${info.version}${info.path}`;
+  return `https://jmfrancois.github.io/cdn-assets/dist/${info.name}/${info.version}${info.path}`;
 }
 
-// moduleToCdn.configure(getURL);
+moduleToCdn.configure(getURL);
 
 module.export = {};
